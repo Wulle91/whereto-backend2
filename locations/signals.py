507ea92f804a44d939135714470164b0848fa761
Wulle91 <mvuckovi91@gmail.com>
2023-07-05 10:41:28 +0000

    Adjust signal to create post if location exists

--- locations/signals.py
+++ locations/signals.py
@@ -5,10 +5,21 @@ from .models import Location
 
 
 @receiver(post_save, sender=Post)
-def create_location(sender, instance, created, **kwargs):
+def create_location(sender, instance, created,
+                    image, owner, created_at, **kwargs):
     if created:
-        Location.objects.get_or_create(
+        location, _ = Location.objects.get_or_create(
             name=instance.name,
             address=instance.address,
             image_url=instance.image_url
+        )
+
+        Post.objects.create(
+            title=instance.title,
+            content=instance.content,
+            name=instance.name,
+            address=instance.address,
+            image=instance.image,
+            owner=instance.owner,
+            created_at=instance.created_at
         )
