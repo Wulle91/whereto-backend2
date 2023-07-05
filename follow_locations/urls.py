997571906ee4e2f3ba238ef0c9624792bc7dd3ef
Wulle91 <mvuckovi91@gmail.com>
2023-07-05 18:56:24 +0000

    Adjsut urls

--- follow_locations/urls.py
+++ follow_locations/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from followers import views
 
 urlpatterns = [
-    path('followers/', views.FollowLocationList.as_view()),
-    path('followers/<int:pk>/', views.FollowLocationDetail.as_view())
+    path('follow/', views.FollowLocationList.as_view()),
+    path('follow/<int:pk>/', views.FollowLocationDetail.as_view())
 ]
