560e8c82044e516157bd2d494e630b9860d814e4
Wulle91 <mvuckovi91@gmail.com>
2023-07-05 14:36:47 +0000

    Add signal to followers

--- followers/signals.py
+++ followers/signals.py
@@ -1,20 +1,17 @@
 import random
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Follower
 from locations.models import Location
 
 
-@receiver(post_save, sender=Follower)
-def populate_missing_fields(sender, instance, created, **kwargs):
-    if created:
-        if not instance.followed:
-            random_user = User.objects.order_by('?').values_list('pk', flat=True).first()
-            instance.followed_id = random_user
+@receiver(pre_save, sender=Follower)
+def populate_missing_fields(sender, instance, **kwargs):
+    if not instance.followed:
+        random_user = User.objects.order_by('?').first()
+        instance.followed = random_user
 
-        if not instance.followed_location:
-            random_location = Location.objects.order_by('?').values_list('pk', flat=True).first()
-            instance.followed_location_id = random_location
-
-        instance.save()
+    if not instance.followed_location:
+        random_location = Location.objects.order_by('?').first()
+        instance.followed_location = random_location
