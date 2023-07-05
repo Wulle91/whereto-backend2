0688ed9d5aae2219cde5e90be373a254e827c390
Wulle91 <mvuckovi91@gmail.com>
2023-07-05 15:12:55 +0000

    Add signal to followers

--- followers/apps.py
+++ followers/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class FollowersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'followers'
-
-    def ready(self):
-        import followers.signals
