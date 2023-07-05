693ed08570d82462b80864aa80ff92f8b3962324
Wulle91 <mvuckovi91@gmail.com>
2023-07-05 10:55:52 +0000

    Adjust signal to create only locations if not existing

--- locations/signals.py
+++ locations/signals.py
@@ -5,21 +5,11 @@ from .models import Location
 
 
 @receiver(post_save, sender=Post)
-def create_location(sender, instance, created,
-                    image, owner, created_at, **kwargs):
+def create_location(sender, instance, created, **kwargs):
     if created:
-        location, _ = Location.objects.get_or_create(
-            name=instance.name,
-            address=instance.address,
-            image_url=instance.image_url
-        )
-
-        Post.objects.create(
-            title=instance.title,
-            content=instance.content,
-            name=instance.name,
-            address=instance.address,
-            image=instance.image,
-            owner=instance.owner,
-            created_at=instance.created_at
-        )
+        if Location.DoesNotExist:
+            location = Location.objects.create(
+                name=instance.name,
+                address=instance.address,
+                image_url=instance.image_url
+            )
