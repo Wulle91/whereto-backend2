2ff37e5722211f0860bb6157541770e18c18c4b0
Wulle91 <mvuckovi91@gmail.com>
2023-07-06 11:00:01 +0000

    Set back profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -7,7 +7,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
-    following_location_id = serializers.SerializerMethodField()
     posts_count = serializers.ReadOnlyField()
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
