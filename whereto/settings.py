929f15cea8d37bfd7240ac68a9a6a6c18cecceaf
Wulle91 <mvuckovi91@gmail.com>
2023-07-08 06:34:13 +0000

    Debug False

--- whereto/settings.py
+++ whereto/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'again-moments-d2bdd3c5ef83.herokuapp.com',
